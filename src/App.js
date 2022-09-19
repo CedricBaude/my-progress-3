e8def11a8fddfba8f5ee5bbbb78ec14e61d396ba
CedricBaude <baude.cedric.perso@gmail.com>
2022-09-19 19:46:25 +0200

    add routing and nav component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import Service from './pages/Service';
+import Contact from './pages/Contact';
+import Error from './_utils/Error';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<Home />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/service" element={<Service />} />
+					<Route path="/contact" element={<Contact />} />
+
+					<Route path="*" element={<Error />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/public/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            Home works !
+        </div>
+    );
+};
+
+export default Home;
