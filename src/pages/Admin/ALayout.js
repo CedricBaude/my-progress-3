acaca0d75805d2ceac78df0821bcde0f42783b05
CedricBaude <baude.cedric.perso@gmail.com>
2022-09-20 19:26:56 +0200

    admin routing

--- src/pages/Admin/ALayout.js
+++ src/pages/Admin/ALayout.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from '../../components/admin/Header';
+import SideMenu from '../../components/admin/SideMenu';
 
+import "./admin.css";
 
 const ALayout = () => {
     return (
         <div className='ALayout'>
-            Layout Admin
-            <Outlet />
+            <Header />
+            <div id="admin">
+                <SideMenu />
+                <div id="admin-body">
+                    <Outlet />
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Admin/AdminRouter.js
+++ src/pages/Admin/AdminRouter.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
-import { ALayout, Dashboard } from "../../pages/Admin"
+import { ALayout, Dashboard } from "../../pages/Admin";
+import { User, UEdit, Add } from '../Admin/User';
+import { Cocktail, CEdit } from '../Admin/Cocktail';
+import Error from '../../_utils/Error';
 
 const AdminRouter = () => {
     return (
         <Routes>
             <Route element={<ALayout />} >
                 <Route path="dashboard" element={<Dashboard />} />
+                <Route path="user">
+                    <Route path="index" element={<User />} />
+                    <Route path="edit" element={<UEdit />} />
+                    <Route path="add" element={<Add />} />
+                </Route>
+                <Route path="*" element={<Error />} />
             </Route>
         </Routes>
     );
