971ed1721a92e6014ce429bf13cae3650e12552d
CedricBaude <baude.cedric.perso@gmail.com>
2022-09-20 15:10:18 +0200

    routing optimisation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home';
-import Service from './pages/Service';
-import Contact from './pages/Contact';
-import Error from './_utils/Error';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, } from 'react-router-dom';
+import PublicRoute from './pages/Public/PublicRoute';
+
 
 function App() {
 	return (
 		<div className="App">
 			<BrowserRouter>
-				<Routes>
-					<Route index element={<Home />} />
-					<Route path="/home" element={<Home />} />
-					<Route path="/service" element={<Service />} />
-					<Route path="/contact" element={<Contact />} />
-
-					<Route path="*" element={<Error />} />
-				</Routes>
+				<PublicRoute />
 			</BrowserRouter>
 		</div>
 	);
