8faab205ab6e4b0a96b3d532e5ee6a5fdd7cea03
CedricBaude <baude.cedric.perso@gmail.com>
2022-09-21 16:56:23 +0200

    start auth

--- src/pages/Admin/User/UserEdit.js
+++ src/pages/Admin/User/UserEdit.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const UserEdit = () => {
+
+    let params = useParams();
+    console.log(params);
+
     return (
         <div className='UserEdit'>
-
+            User Edit
         </div>
     );
 };
