b21bd7733aca2a18bc3dca19bbf17622c606d884
CedricBaude <baude.cedric.perso@gmail.com>
2022-09-22 15:35:16 +0200

    authguard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PublicRouter from './pages/Public/PublicRouter';
 import AdminRouter from './pages/Admin/AdminRouter';
 import AuthRouter from './pages/Auth/AuthRouter';
+import AuthGuard from './_helpers/AuthGuard';
 
 
 function App() {
@@ -12,7 +13,12 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route path="/*" element={<PublicRouter />} />
-					<Route path="/admin/*" element={<AdminRouter />} />
+					<Route path="/admin/*" element={
+						<AuthGuard>
+							<AdminRouter />
+						</AuthGuard>
+
+					} />
 					<Route path="/auth/*" element={<AuthRouter />} />
 				</Routes>
 			</BrowserRouter>
