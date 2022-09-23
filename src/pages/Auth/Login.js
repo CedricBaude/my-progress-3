e0fff4782156063589f17ed5608cb54a36d02624
CedricBaude <baude.cedric.perso@gmail.com>
2022-09-23 12:29:13 +0200

    useState/onChange form value

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -2,23 +2,33 @@ import React, { useState } from 'react';
 import "./auth.css";
 
 const Login = () => {
-    const [login, setLogin] = useState('');
-    const [password, setPassword] = useState('');
+    // const [login, setLogin] = useState('');
+    // const [password, setPassword] = useState('');
+    const [credentials, setCredentials] = useState({
+        login: '',
+        password: ''
+    })
+
+    const onChange = (e) => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        });
+    }
 
     const onSubmit = (e) => {
         e.preventDefault();
-        console.log('formulaire');
-        console.log(login, password);
+        console.log(credentials);
     }
     return (
         <form action="" onSubmit={onSubmit}>
             <div className="group">
                 <label htmlFor="login">Identifiant</label>
-                <input type="text" name="login" value={login} onChange={e => setLogin(e.target.value)} />
+                <input type="text" name="login" value={credentials.login} onChange={onChange} />
             </div>
             <div className="group">
                 <label htmlFor="password">Mot de passe</label>
-                <input type="text" name='password' value={password} onChange={e => setPassword(e.target.value)} />
+                <input type="text" name='password' value={credentials.password} onChange={onChange} />
             </div>
             <div className="group">
                 <button>Connexion</button>
