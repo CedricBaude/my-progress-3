0f6662ee0f91cae8bf7ce9c3bbffc2a473cdfdd3
CedricBaude <baude.cedric.perso@gmail.com>
2022-09-23 18:17:49 +0200

    add axios

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 import "./auth.css";
 
@@ -5,7 +6,7 @@ const Login = () => {
     // const [login, setLogin] = useState('');
     // const [password, setPassword] = useState('');
     const [credentials, setCredentials] = useState({
-        login: '',
+        email: 'myrl51@hotmail.com',
         password: ''
     })
 
@@ -19,12 +20,16 @@ const Login = () => {
     const onSubmit = (e) => {
         e.preventDefault();
         console.log(credentials);
+        axios.post(' https://api.fake-rest.refine.dev/users/1', credentials)
+            .then(res => console.log(res))
+            .catch(error => console.log(error))
     }
+
     return (
         <form action="" onSubmit={onSubmit}>
             <div className="group">
                 <label htmlFor="login">Identifiant</label>
-                <input type="text" name="login" value={credentials.login} onChange={onChange} />
+                <input type="text" name="email" value={credentials.email} onChange={onChange} />
             </div>
             <div className="group">
                 <label htmlFor="password">Mot de passe</label>
